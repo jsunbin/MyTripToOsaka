c1a113b0ace405fd651fe18f487f73b60a2908ff
jsunbin <jsnb6n@gmail.com>
2023-09-09 23:25:05 +0900

    Begin: 프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MainPage from "./pages/MainPage";
+import { ResetStyle } from "./styles/ResetStyle";
+
+function App() {
+  return (
+    <>
+    <ResetStyle/>
+    <MainPage />
+    </>
+  );
+}
+export default App;
